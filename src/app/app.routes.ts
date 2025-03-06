35a45b9d6955063b2ffbb1ad090dd9e3893f897a
Asyl <duldievasylbek@gmail.com>
2025-03-06 10:11:25 +0500

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', 
+    component:LayoutComponent,
+    children:[
+        {path: '', component: HomeComponent},
+        {path: "login", component: LoginComponent}
+    ] }
+
+];
