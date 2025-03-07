aabee30bcd78c3fdc47afe5412db06a3647831a2
Asyl <duldievasylbek@gmail.com>
2025-03-07 17:04:35 +0500

    land/swip

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
-import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-    {path: '', 
-    component:LayoutComponent,
-    children:[
-        {path: '', component: HomeComponent},
-        {path: "login", component: LoginComponent}
-    ] }
+    { path: '', component: LandingComponent },
+
+    {path: 'login', component: LoginComponent}
 
 ];
---
+++ src/app/pages/swip/swip.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-swip',
+  imports: [],
+  templateUrl: './swip.component.html',
+  styleUrl: './swip.component.scss'
+})
+export class SwipComponent {
+
+}
