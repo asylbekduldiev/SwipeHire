45e370296cc8f9540c69d6eb09414f0e92cd2a2d
Asyl <duldievasylbek@gmail.com>
2025-03-12 23:42:32 +0500

    auth-modal-gsap-animation

--- src/app/pages/landing/auth/auth-modal.component.ts
+++ src/app/pages/landing/auth/auth-modal.component.ts
@@ -1,18 +1,48 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild, AfterViewInit, OnChanges, SimpleChanges } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-auth-modal',
-  imports: [],
   templateUrl: './auth-modal.component.html',
   styleUrl: './auth-modal.component.scss'
 })
-export class AuthModalComponent {
-  @Output() close = new EventEmitter<void>()
+export class AuthModalComponent implements AfterViewInit, OnChanges {
+  @Output() close = new EventEmitter<void>();
+  @ViewChild('modalOverlay', { static: false }) modalOverlay!: ElementRef;
+  @ViewChild('modalContent', { static: false }) modalContent!: ElementRef;
 
-  closeModal(){
-    this.close.emit()
+  ngAfterViewInit() {
+    this.animateOpen();
   }
-  // loginWithGoogle(){
 
-  // }
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['isOpen'] && changes['isOpen'].currentValue) {
+      this.animateOpen();
+    }
+  }
+
+  closeModal() {
+    this.animateClose();
+  }
+
+  private animateOpen() {
+    gsap.fromTo(this.modalOverlay.nativeElement, 
+      { opacity: 0 }, 
+      { opacity: 1, duration: 0.3 }
+    );
+
+    gsap.fromTo(this.modalContent.nativeElement, 
+      { opacity: 0, scale: 0.9, y: -30 }, 
+      { opacity: 1, scale: 1, y: 0, duration: 0.3, ease: "power2.out" }
+    );
+  }
+
+  private animateClose() {
+    gsap.to(this.modalContent.nativeElement, { 
+      opacity: 0, scale: 0.9, y: -30, duration: 0.2, ease: "power2.in", 
+      onComplete: () => this.close.emit()
+    });
+
+    gsap.to(this.modalOverlay.nativeElement, { opacity: 0, duration: 0.2 });
+  }
 }
