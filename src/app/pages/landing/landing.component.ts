5cebb38a6b6f155d075eb8d9309ebd911db64c65
Asyl <duldievasylbek@gmail.com>
2025-03-21 20:12:09 +0500

    fixed set signal

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,31 +1,31 @@
-import { Component, effect, signal, Signal } from '@angular/core';
+import { Component, effect, signal, computed, WritableSignal } from '@angular/core';
 import { AuthModalComponent } from './auth/auth-modal.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
+  standalone: true,
   imports: [CommonModule, AuthModalComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
-  isModalOpen = false
-  lastScrollTop = 0
-  isHeaderVisible: Signal<boolean> = signal(true)
-
+  isModalOpen = false;
+  lastScrollTop = 0;
+  isHeaderVisible: WritableSignal<boolean> = signal(true);
 
   constructor() {
     effect(() => {
       window.addEventListener('scroll', () => {
         const scrollTop = window.scrollY;
-        this.isHeaderVisible.set(scrollTop < this.lastScrollTop);
+        this.isHeaderVisible.set(scrollTop < this.lastScrollTop); 
         this.lastScrollTop = scrollTop;
       });
     });
   }
 
   openModal() {
-    this.isModalOpen = true
+    this.isModalOpen = true;
   }
   
   closeModal() {
