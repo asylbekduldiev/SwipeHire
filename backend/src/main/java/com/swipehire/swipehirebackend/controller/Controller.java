0a9a655252fbc6db40f7093ecb630d919d12c80b
Asyl <duldievasylbek@gmail.com>
2025-04-18 18:22:56 +0500

    Starting UserDetailsService

--- backend/src/main/java/com/swipehire/swipehirebackend/controller/Controller.java
+++ backend/src/main/java/com/swipehire/swipehirebackend/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.swipehire.swipehirebackend;
+package com.swipehire.swipehirebackend.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
